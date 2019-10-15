63f7ab05fc4e63d132b6eacde116051f609a27c1
mRcfps <1402491442@qq.com>
2019-10-15 21:12:49 +0800

    数据展示：实现食谱列表

---
+++ client/components/RecipeCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card recipe-card">
+    <img :src="recipe.picture" class="card-img-top" />
+    <div class="card-body">
+      <h5 class="card-title">{{ recipe.name }}</h5>
+      <p class="card-text">
+        <strong>成分:</strong>
+        {{ recipe.ingredients }}
+      </p>
+      <div class="action-buttons">
+        <nuxt-link :to="`/recipes/${recipe.id}/`" class="btn btn-sm btn-success">查看</nuxt-link>
+        <nuxt-link :to="`/recipes/${recipe.id}/edit/`" class="btn btn-sm btn-primary">编辑</nuxt-link>
+        <button @click="onDelete(recipe.id)" class="btn btn-sm btn-danger">删除</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["recipe", "onDelete"]
+};
+</script>
+
+<style>
+.card-img-top {
+  height: 12rem;
+  width: 100%;
+}
+
+.recipe-card {
+  border: none;
+  box-shadow: 0 1rem 1.5rem rgba(0, 0, 0, 0.6);
+}
+</style>
---
+++ client/pages/recipes/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <main class="container mt-5">
+    <div class="row">
+      <div class="col-12 text-right mb-4">
+        <div class="d-flex justify-content-between">
+          <h3>吃货天堂</h3>
+          <nuxt-link to="/recipes/add" class="btn btn-info">添加食谱</nuxt-link>
+        </div>
+      </div>
+      <template v-for="recipe in recipes">
+        <div :key="recipe.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <recipe-card :onDelete="deleteRecipe" :recipe="recipe"></recipe-card>
+        </div>
+      </template>
+    </div>
+  </main>
+</template>
+
+<script>
+import RecipeCard from "~/components/RecipeCard.vue";
+
+const sampleData = [
+  {
+    id: 1,
+    name: "通心粉",
+    picture: "/images/food-1.jpeg",
+    ingredients: "牛肉, 猪肉, 羊肉",
+    difficulty: "easy",
+    prep_time: 15,
+    prep_guide:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, porro. Dignissimos ducimus ratione totam fugit officiis blanditiis exercitationem, nisi vero architecto quibusdam impedit, earum "
+  },
+  {
+    id: 2,
+    name: "羊肉串",
+    picture: "/images/food-2.jpeg",
+    ingredients: "牛肉, 猪肉, 羊肉",
+    difficulty: "easy",
+    prep_time: 15,
+    prep_guide:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, porro. Dignissimos ducimus ratione totam fugit officiis blanditiis exercitationem, nisi vero architecto quibusdam impedit, earum "
+  },
+  {
+    id: 3,
+    name: "炒饭",
+    picture: "/images/banner.jpg",
+    ingredients: "牛肉, 猪肉, 羊肉",
+    difficulty: "easy",
+    prep_time: 15,
+    prep_guide:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, porro. Dignissimos ducimus ratione totam fugit officiis blanditiis exercitationem, nisi vero architecto quibusdam impedit, earum "
+  }
+];
+
+export default {
+  head() {
+    return {
+      title: "食谱列表"
+    };
+  },
+  components: {
+    RecipeCard
+  },
+  asyncData(context) {
+    let data = sampleData;
+    return {
+      recipes: data
+    };
+  },
+  data() {
+    return {
+      recipes: []
+    };
+  },
+  methods: {
+    deleteRecipe(recipe_id) {
+      console.log(deleted`${recipe.id}`);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
